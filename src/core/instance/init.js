d3f1fa00777fac862ba5ac87903843a578feae27
杨春 <894584322@qq.com>
2021-03-15 15:33:15 +0800

    ‘mark’

--- src/core/instance/init.js
+++ src/core/instance/init.js
@@ -11,7 +11,7 @@ import { initProvide, initInjections } from './inject'
 import { extend, mergeOptions, formatComponentName } from '../util/index'
 
 let uid = 0
-
+// 测试一下vscode覆盖问题
 export function initMixin (Vue: Class<Component>) {
   Vue.prototype._init = function (options?: Object) {
     const vm: Component = this
